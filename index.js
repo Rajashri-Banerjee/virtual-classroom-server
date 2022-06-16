1d5e5a492954bf028e36c46e99506a0306813167
Rajashri Banerjee <rajashri.rajashri1980@gmail.com>
2022-06-16 12:42:48 +0530

    Some changes

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ const userRouter = require ('./src/routes/user')
 const teacherRouter = require ('./src/routes/teacher')
 const dotenv = require ('dotenv');
 const morgan = require('morgan');
+const require = require('cors');
 
 const app = express()
 app.use(express.json())
 app.use(morgan('common'))
+app.use(cors)
 dotenv.config();
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  const allowedOrigins = [
-      "http://localhost:3000",
-  ];
-  const origin = req.headers.origin;
-    if (true) {
-        res.setHeader('Access-Control-Allow-Origin', "*");
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Website you wish to allow to connect
+//   const allowedOrigins = [
+//       "http://localhost:3000",
+//   ];
+//   const origin = req.headers.origin;
+//     if (true) {
+//         res.setHeader('Access-Control-Allow-Origin', "*");
+//     }
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Set-Cookie,Authorization,authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Set-Cookie,Authorization,authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of 
-    next();
-  });
+//     // Pass to next layer of 
+//     next();
+//   });
 
 app.use(userRouter)
 app.use(teacherRouter)
